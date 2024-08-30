0ca93b5b601fb50abb56ec8a09e75e03162e422b
Zelalem Mekonnen <mekonnen.zelalem251@gmail.com>
2024-08-30 20:40:38 +0300

    Fix errors

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,11 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectToDatabase = async () => {
-  const { MONGO_URI } = process.env;
-  try {
-    await mongoose.connect(MONGO_URI || 'mongodb://localhost:27017/songs');
+const mongoUri =
+  process.env.MONGO_URI || 'mongodb://localhost:27017/song-api-db';
 
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(mongoUri);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Failed to connect to MongoDB', error);
